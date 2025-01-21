5522090576b7fe96ef93b3e8798749fcab644f21
Juan <juancmendonca@gmail.com>
2025-01-21 17:18:09 -0300

    /release: version 1.0

--- src/main/java/com/juandev/workshop_mongo/repository/PostRepository.java
+++ src/main/java/com/juandev/workshop_mongo/repository/PostRepository.java
@@ -3,12 +3,22 @@ package com.juandev.workshop_mongo.repository;
 import com.juandev.workshop_mongo.domain.Post;
 import com.juandev.workshop_mongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContaining(String text );
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    @Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } } ," +
+            "{ $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }," +
+            "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/juandev/workshop_mongo/services/PostService.java
+++ src/main/java/com/juandev/workshop_mongo/services/PostService.java
@@ -9,6 +9,7 @@ import com.juandev.workshop_mongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -25,6 +26,11 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return repository.findByTitleContaining(text);
+        return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
